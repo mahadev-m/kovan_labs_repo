8cafebb0962dd2c8572a26a45a68a764d13877e6
mahadev-m <76099581+mahadev-m@users.noreply.github.com>
2021-01-04 11:49:05 +0530

    Add files via upload

---
+++ Employee Details/reg.js
@@ -0,0 +1,141 @@
+
+
+function formValidation()
+{
+var uid = document.registration.userid;
+var passid = document.registration.passid;
+var uname = document.registration.username;
+var uadd = document.registration.address;
+var ucountry = document.registration.country;
+var uzip = document.registration.zip;
+var uemail = document.registration.email;
+var umsex = document.registration.msex;
+var ufsex = document.registration.fsex; 
+if(userid_validation(uid,5,12))
+{
+if(passid_validation(passid,7,12))
+{
+if(allLetter(uname))
+{
+if(alphanumeric(uadd))
+{ 
+if(countryselect(ucountry))
+{
+if(allnumeric(uzip))
+{
+if(ValidateEmail(uemail))
+{
+if(validsex(umsex,ufsex))
+{
+}
+} 
+}
+} 
+}
+}
+}
+}
+return false;
+
+ if (document.registration.username.value == "") {
+                alert("Please fill in the 'Your Employee Name' box.");
+                return false;
+            }
+            if (document.registration.userid.value == "") {
+                alert("Enter Employee Number");
+                return false;
+            }
+ 
+ 
+ 
+            if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(document.registration.email.value)) {
+                alert("You have entered an invalid email address!")
+                return (false);
+
+            
+
+            
+           
+           
+           
+
+}
+
+function results(registration)
+{
+  var pw1 = document.getElementById("pswd1");
+  var pw2 = document.getElementById("pswd2");
+  if(pw1 != pw2)
+  {	
+  	alert("Passwords did not match");
+  } else {
+  	alert("Password created successfully");
+  }
+}
+  
+  return true;
+}
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+
+myInput.onkeyup = function() {
+
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+alert("sucessfully Submitted");
+function display()
+{
+  var uid = document.registration.userid;
+	document.write("Employee ID:"+uid);
+}
+
